e64408a738099566382342b10d976b4cc6f3bab5
Kate Schumacher <kate.schumacher@gmail.com>
2024-11-29 16:37:13 -0600

    Add new functions to gameboard object

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 
 
 const gameboard = (function () {
-    const _array = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-    ];
+    let _array = [];
+    const rows = 3;
+    const columns = 3;
+
+    //Initialize game board with empty cells
+
+    for (let i = 0; i < rows; i++) {
+        _array[i] = [];
+        for (let j = 0; j < columns; j++) {
+            _array[i].push(Cell());
+        }
+    }
 
     const getGameboard = () => _array;
 
-    const printGameboard = () => console.log(_array);
+    const printGameboard = () => {
+        const filledBoard = _array.map((row) => row.map((cell) => cell.getValue()));
+        // console.log(filledBoard);
+    };
 
     const makeMove = (player, cellRow, cellColumn) => {
-        let activeCell = _array[cellRow][cellColumn];
-        if (activeCell === "X" || activeCell === "O") return;
-        if (player == 1) {
-            activeCell = "X";
-        } else if (player == 2) {
-            activeCell = "O";
+        // console.log("Invoking makeMove");
+        // console.log("Making activeCell the one at " + cellRow +", " + cellColumn);
+        // console.log("Console logging gameboard[cellRow] in the next line...");
+        // console.log(_array[cellRow][cellColumn]);
+        let activeCell = _array[cellRow][cellColumn].getValue();
+        if (activeCell === 1 || activeCell === 2) {
+            // console.log("activeCell equals 1 or 2, exiting makeMove");
+            return;
         }
-        _array[cellRow][cellColumn] = activeCell;
+        _array[cellRow][cellColumn].fillCell(player);
+        // console.log("Exiting makeMove after filling cell");
     }
 
-    return {getGameboard, printGameboard, makeMove};
+    const checkWin = (player, cellRow, cellColumn) => {
+        let cellJustPlayed = _array[cellRow][cellColumn];
+    }
+
+    return { getGameboard, printGameboard, makeMove, checkWin };
 })();
+
+function Cell() {
+    // console.log("Invoking Cell()");
+    let value = 0;
+
+    const fillCell = (player) => {
+        // console.log("Invoking fillCell");
+        value = player;
+        // console.log("Value should now be " + player);
+        // console.log("Value is actually " + value);
+    }
+
+    const getValue = () => {
+        // console.log("Invoking getValue");
+        return value;
+    }
+
+    return { fillCell, getValue };
+}
