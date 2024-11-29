8d2a7ed894700b01bfb8c10243e89ea3ae42f90d
Kate Schumacher <kate.schumacher@gmail.com>
2024-11-29 13:24:29 -0600

    Write basic function to make a move

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
 
-let gameboard = (function(){
-    let array = [
+const gameboard = (function () {
+    const _array = [
         [0,0,0],
         [0,0,0],
         [0,0,0]
     ];
+
+    const getGameboard = () => _array;
+
+    const makeMove = (cellRow, cellColumn) => {
+        _array[cellRow][cellColumn] = "X";
+    }
+
+    return {getGameboard, makeMove};
 })();
