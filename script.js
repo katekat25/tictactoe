4d23577e2a88f38349781793b858e218e36b57d1
Kate Schumacher <kate.schumacher@gmail.com>
2024-12-04 13:08:43 -0600

    Disallow replacing marks on the board

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ const gameboard = (function () {
 
     const getGameboard = () => _array;
 
+    const getValueAtCoord = (x,y) => _array[x][y].getValue();
+
     const printGameboard = () => {
         const filledBoard = _array.map((row) => row.map((cell) => cell.getValue()));
         console.log(filledBoard);
@@ -54,44 +56,39 @@ const gameboard = (function () {
 
     function checkWin() {
         function getCoord(x, y) {
-            // console.log("in getCoord");
-            // console.log("winningCombos[x] is " + winningCombos[x]);
-            // console.log("winningCombos[x][y] is " + winningCombos[x][y]);
             let coord = winningCombos[x][y]
                 .toString()
                 .split(",");
             coord[0] = Number(coord[0]);
             coord[1] = Number(coord[1]);
-            // console.log("Returning coord as " + coord);
             return coord;
         }
         let roundCount = playerController.getRoundCount();
+        //if first player has had time to play at least 3 tokens
         if (roundCount >= 4 && roundCount <= 8) {
             let activePlayerValue = playerController.getActivePlayer().playerValue;
+            //loop through winningCombos and check if value at each coord equals the current player's value
             for (i = 0; i < winningCombos.length; i++) {
                 let coord = getCoord(i, 0);
-                // console.log("winningCombos[" + i + ",0] is " + winningCombos[i, 0]);
                 if (_array[coord[0]][coord[1]].getValue() == activePlayerValue) {
-                    // console.log("These first coords match.");
                     coord = getCoord(i, 1);
                     if (_array[coord[0]][coord[1]].getValue() == activePlayerValue) {
-                        // console.log("These second coords match.");
                         coord = getCoord(i, 2);
                         if (_array[coord[0]][coord[1]].getValue() == activePlayerValue) {
-                            // console.log("These third coords match.");
                             console.log("Player " + activePlayerValue + " wins!");
                             return;
                         }
                     }
                 }
             }
+            //if board is filled with no win
             if (roundCount == 8) {
                 console.log("It's a tie.");
             }
         }
     }
 
-    return { getGameboard, printGameboard, clearGameboard, makeMove, checkWin };
+    return { getGameboard, getValueAtCoord, printGameboard, clearGameboard, makeMove, checkWin };
 })();
 
 function Cell() {
@@ -131,12 +128,16 @@ const playerController = (function () {
     const getRoundCount = () => roundCount;
 
     function playRound(row, column) {
-        console.log(`${getActivePlayer().name}'s turn.`);
-        gameboard.makeMove(activePlayer.playerValue, row, column);
-        console.log(gameboard.printGameboard());
-        gameboard.checkWin();
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-        roundCount++;
+        if (gameboard.getValueAtCoord(row, column) == 0) {
+            console.log(`${getActivePlayer().name}'s turn.`);
+            gameboard.makeMove(activePlayer.playerValue, row, column);
+            console.log(gameboard.printGameboard());
+            gameboard.checkWin();
+            activePlayer = activePlayer === players[0] ? players[1] : players[0];
+            roundCount++;
+        } else {
+            console.log("A move has already been made at that square. Please try again.");
+        }
     }
 
     const resetGame = () => {
